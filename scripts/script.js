3885ace4ef86ae9e46f4082c247dda3411ddb007
hojinchang <72773536+hojinchang@users.noreply.github.com>
2023-10-28 18:15:17 -0700

    Add extra media queries for header section
    Added media queries which increase the text size of the nav links and welcome message.

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,6 @@ function displayIntroBanner(idx) {
     if (idx < introBannerText.length) {
         setTimeout(() => {
             introBannerText[idx].style.opacity = "1";
-            // introBannerText[idx].style.scale = "1";
             displayIntroBanner(idx + 1);
         }, delay);
     }
