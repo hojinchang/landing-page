68e1ace36ec9181e30dd8375ecce7b934a81bb9f
Ho Jin Chang <72773536+hojinchang@users.noreply.github.com>
2023-10-28 17:07:27 -0700

    Init files

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const introBannerText = document.querySelectorAll(".intro-banner p");
+
+// This function animates the intro banner and creates a delay between the text elements showing
+function displayIntroBanner(idx) {
+    let delay;
+    idx === 0 ? delay=1000 : delay=2000;
+
+    // resursive
+    if (idx < introBannerText.length) {
+        setTimeout(() => {
+            introBannerText[idx].style.opacity = "1";
+            introBannerText[idx].style.transform = "translateY(0)";
+            displayIntroBanner(idx + 1);
+        }, delay);
+    }
+}
+displayIntroBanner(0);
