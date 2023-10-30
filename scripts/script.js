92ca187fa981c7a82bf5ef7ccf8fb6c6a1dfeb1a
hojinchang <72773536+hojinchang@users.noreply.github.com>
2023-10-29 22:11:06 -0700

    Add swooping entry effect to project containers

--- scripts/script.js
+++ scripts/script.js
@@ -2,53 +2,53 @@ window.addEventListener("beforeunload", () => {
     window.scrollTo(0, 0);
 })
 
-const introBannerAnimation = (() => {
-    const introBannerElements = document.querySelectorAll(".intro-banner > *");
-    const navigationLinks = document.querySelectorAll(".header-menu a");
+// const introBannerAnimation = (() => {
+//     const introBannerElements = document.querySelectorAll(".intro-banner > *");
+//     const navigationLinks = document.querySelectorAll(".header-menu a");
 
-    // Disables the navigation links
-    const _disableNavLinks = () => {
-        const _disableNavigation = (e) => {
-            e.preventDefault();
-        }
+//     // Disables the navigation links
+//     const _disableNavLinks = () => {
+//         const _disableNavigation = (e) => {
+//             e.preventDefault();
+//         }
 
-        navigationLinks.forEach(nav => {
-            nav.addEventListener("click", _disableNavigation)
-        })
-    }
+//         navigationLinks.forEach(nav => {
+//             nav.addEventListener("click", _disableNavigation)
+//         })
+//     }
 
-    // Enables the navigation links
-    const _enableNavLinks = () => {
-        navigationLinks.forEach(nav => {
-            nav.removeEventListener("click", _disableNavLinks);
-        })
-    }
+//     // Enables the navigation links
+//     const _enableNavLinks = () => {
+//         navigationLinks.forEach(nav => {
+//             nav.removeEventListener("click", _disableNavLinks);
+//         })
+//     }
 
-    // This function animates the intro banner and creates a delay between the text elements showing
-    const _displayIntroBanner = (idx) => {
-        let delay;
-        idx === 0 ? delay=1000 : delay=2000;  // Make the delay for the first animation shorter than the rest
+//     // This function animates the intro banner and creates a delay between the text elements showing
+//     const _displayIntroBanner = (idx) => {
+//         let delay;
+//         idx === 0 ? delay=1000 : delay=2000;  // Make the delay for the first animation shorter than the rest
 
-        // resursive
-        if (idx < introBannerElements.length) {
-            setTimeout(() => {
-                introBannerElements[idx].style.opacity = "1";
-                _displayIntroBanner(idx + 1);
+//         // resursive
+//         if (idx < introBannerElements.length) {
+//             setTimeout(() => {
+//                 introBannerElements[idx].style.opacity = "1";
+//                 _displayIntroBanner(idx + 1);
 
-                if (idx === introBannerElements.length-1) {
-                    // Enable scrolling and navigation links after the last animation is finsihed
-                    document.body.style.overflow = "auto";
-                    _enableNavLinks();
-                }
-            }, delay);
-        }
-    }
+//                 if (idx === introBannerElements.length-1) {
+//                     // Enable scrolling and navigation links after the last animation is finsihed
+//                     document.body.style.overflow = "auto";
+//                     _enableNavLinks();
+//                 }
+//             }, delay);
+//         }
+//     }
 
-    // Make website unscrollable while animation is playing
-    document.body.style.overflow = "hidden";
-    _disableNavLinks();
-    _displayIntroBanner(0);
-})();
+//     // Make website unscrollable while animation is playing
+//     document.body.style.overflow = "hidden";
+//     _disableNavLinks();
+//     _displayIntroBanner(0);
+// })();
 
 
 const projectSelection = (() => {
@@ -83,29 +83,59 @@ const projectSelection = (() => {
         e.target.classList.add("active");
     }
 
+    // Activate and display the project container
     const _activateProject = (e, project) => {
         if (!_active(e)) {
             _setActive(e);
             project.style.display = "flex";
         }
     }
+    
+    let previousBtnIdx = 0;  // Initial position of the projects container 
+    const _swoopDirection = (e) => {
+        const selectedBtn = e.target;
+
+        // Determine the index of the selected project selector
+        const selectedBtnIdx = Array.from(projectSelectorBtns).findIndex(btn => btn === selectedBtn);
+        // Select the corresponding project container
+        const selectedProjectContainer = projectContainers[selectedBtnIdx];
+
+        // Remove the swoop direction state classes
+        projectContainers.forEach(project => {
+            project.classList.remove("swoop-right", "swoop-left");
+        })
+
+        // If the selected project selector idx is greater than the previously selected project, 
+        // make the project container swoop in from the right.
+        // Else, make it swoop from the left.
+        selectedBtnIdx > previousBtnIdx ? 
+            selectedProjectContainer.classList.add("swoop-right") : 
+            selectedProjectContainer.classList.add("swoop-left");
+
+        previousBtnIdx = selectedBtnIdx;
+    }
 
     htmlCssBtn.addEventListener("click", (e) => {
+        _swoopDirection(e);
         _activateProject(e, htmlCssProjects);
     })
 
     javascriptBtn.addEventListener("click", (e) => {
+        _swoopDirection(e);
         _activateProject(e, javascriptProjects);
     })
 
     wordpressBtn.addEventListener("click", (e) => {
-       _activateProject(e, wordpressProjects);
+        _swoopDirection(e);
+        _activateProject(e, wordpressProjects);
     })
 
     onlinestoreBtn.addEventListener("click", (e) => {
-       _activateProject(e, onlineStoresProjects);
+        _swoopDirection(e);
+        _activateProject(e, onlineStoresProjects);
     })
 })();
+
 
 
 const navDownBtn = document.querySelector(".nav-down-btn");
